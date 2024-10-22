447175d8dc972d22c9e04863bedb48467a6b8c1c
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-22 12:54:51 -0400

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("")
+    let arrayReverse = array.reverse()
+    return arrayReverse.join('')
 };
+
+console.log(reverseString('your mom goes to college'))
 
 // Do not edit below this line
 module.exports = reverseString;
