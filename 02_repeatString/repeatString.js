337dc3c564943493059f5a570fa51e97f1c81f6e
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-22 12:31:59 -0400

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-    
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let result = ''
+        for (let i = 0; i < num; i++) {
+            result += string
+        }
+        return result
+    } else {
+        return 'ERROR'
+    }
 };
+
+console.log(repeatString('poop', 5))
 
 // Do not edit below this line
 module.exports = repeatString;
