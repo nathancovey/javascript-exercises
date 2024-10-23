c5e538437010244ebdef36f99f8eb082b345e7d1
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-22 21:13:53 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum = 0
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum
 };
+
+console.log(sumAll(1,5))
 
 // Do not edit below this line
 module.exports = sumAll;
