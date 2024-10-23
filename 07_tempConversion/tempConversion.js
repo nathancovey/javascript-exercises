b9c6e9295da32d2d86b3da226fb428f576ccccb3
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2024-10-23 11:05:50 -0400

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusConvertedTemp = Math.round(((fahrenheitTemp - 32) * 5/9) * 10) / 10;
+  return celsiusConvertedTemp
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100))
+
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitConvertedTemp = Math.round(((celsiusTemp * 9 / 5) + 32) * 10) / 10;
+  return fahrenheitConvertedTemp
 };
+
+console.log(convertToFahrenheit(12))
 
 // Do not edit below this line
 module.exports = {
